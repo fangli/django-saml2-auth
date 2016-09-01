1ca0b80395fdc3c984a18945c7a0843b08a2a5a3
Jacob Haslehurst <jacob@haslehurst.net>
2016-09-01 19:27:50 +1000

    Remove use of url `patterns` as it's removed in Django 1.10

--- django_saml2_auth/urls.py
+++ django_saml2_auth/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import url, patterns
-
+from django.conf.urls import url
+from . import views
 
 app_name = 'django_saml2_auth'
 
-urlpatterns = patterns(
-    'django_saml2_auth.views',
-    url(r'^acs/$', "acs", name="acs"),
-    url(r'^welcome/$', "welcome", name="welcome"),
-    url(r'^denied/$', "denied", name="denied"),
-)
+urlpatterns = [
+    url(r'^acs/$', views.acs, name="acs"),
+    url(r'^welcome/$', views.welcome, name="welcome"),
+    url(r'^denied/$', views.denied, name="denied"),
+]
