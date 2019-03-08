7cd852345c05732b61877eed4270e34055007c64
Jian Yuan Lee <jianyuan@gmail.com>
2019-03-08 10:27:19 +0000

    Fallback to get if get_by_natural_key does not exist

--- django_saml2_auth/views.py
+++ django_saml2_auth/views.py
@@ -173,7 +173,10 @@ def acs(r):
     is_new_user = False
 
     try:
-        target_user = User.objects.get_by_natural_key(user_name)
+        if hasattr(User.objects, 'get_by_natural_key'):
+            target_user = User.objects.get_by_natural_key(user_name)
+        else:
+            target_user = User.objects.get(username=user_name)
         if settings.SAML2_AUTH.get('TRIGGER', {}).get('BEFORE_LOGIN', None):
             import_string(settings.SAML2_AUTH['TRIGGER']['BEFORE_LOGIN'])(user_identity)
     except User.DoesNotExist:
