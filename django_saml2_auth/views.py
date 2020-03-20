3bb7f538808502f03320cff93d32818c07156062
Dylan Gonzales <53017996+dgonzo27@users.noreply.github.com>
2020-03-20 14:07:11 -0500

    Update views.py

--- django_saml2_auth/views.py
+++ django_saml2_auth/views.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-
 from saml2 import (
     BINDING_HTTP_POST,
     BINDING_HTTP_REDIRECT,
@@ -22,10 +21,15 @@ from django.template import TemplateDoesNotExist
 from django.http import HttpResponseRedirect
 from django.utils.http import is_safe_url
 
-from rest_auth.utils import jwt_encode
+from book.models import Organization
+from integration.serializers import ReferralCreatorSerializer
+from refer.utils import PasswordlessAuthBackend
 
+from logging import getLogger
 
-# default User or custom User. Now both will work.
+logger = getLogger('django_saml2_auth.views')
+
+# Default User
 User = get_user_model()
 
 try:
@@ -40,39 +44,34 @@ if parse_version(get_version()) >= parse_version('1.7'):
 else:
     from django.utils.module_loading import import_by_path as import_string
 
-
 def _default_next_url():
     if 'DEFAULT_NEXT_URL' in settings.SAML2_AUTH:
         return settings.SAML2_AUTH['DEFAULT_NEXT_URL']
-    # Lazily evaluate this in case we don't have admin loaded.
-    return get_reverse('admin:index')
-
+    else:
+        return get_reverse('start_page')
 
 def get_current_domain(r):
     if 'ASSERTION_URL' in settings.SAML2_AUTH:
         return settings.SAML2_AUTH['ASSERTION_URL']
     return '{scheme}://{host}'.format(
-        scheme='https' if r.is_secure() else 'http',
+        scheme='https' if r.is_secure() else 'http', 
         host=r.get_host(),
     )
 
-
-def get_reverse(objs):
-    '''In order to support different django version, I have to do this '''
+def get_reverse(objects):
     if parse_version(get_version()) >= parse_version('2.0'):
         from django.urls import reverse
     else:
         from django.core.urlresolvers import reverse
-    if objs.__class__.__name__ not in ['list', 'tuple']:
-        objs = [objs]
+    if objects.__class__.__name__ not in ['list', 'tuple']:
+        objects = [objects]
 
-    for obj in objs:
+    for obj in objects:
         try:
             return reverse(obj)
         except:
             pass
-    raise Exception('We got a URL reverse issue: %s. This is a known issue but please still submit a ticket at https://github.com/fangli/django-saml2-auth/issues/new' % str(objs))
-
+    raise Exception('URL reverse issue: %s.  Known issue from fangli/django-saml2-auth' % str(objects))
 
 def _get_metadata():
     if 'METADATA_LOCAL_FILE_PATH' in settings.SAML2_AUTH:
@@ -87,7 +86,6 @@ def _get_metadata():
                 },
             ]
         }
-
 
 def _get_saml_client(domain):
     acs_url = domain + get_reverse([acs, 'acs', 'django_saml2_auth:acs'])
@@ -124,101 +122,125 @@ def _get_saml_client(domain):
     saml_client = Saml2Client(config=spConfig)
     return saml_client
 
-
 @login_required
 def welcome(r):
     try:
-        return render(r, 'django_saml2_auth/welcome.html', {'user': r.user})
+        return render(r, 'django_saml2_auth/tchWelcome.html', {'user': r.user})
     except TemplateDoesNotExist:
         return HttpResponseRedirect(_default_next_url())
-
 
 def denied(r):
     return render(r, 'django_saml2_auth/denied.html')
 
-
 def _create_new_user(username, email, firstname, lastname):
+    logger.debug('_create_new_user')
+    # Create a new user object with the parameters passed
     user = User.objects.create_user(username, email)
     user.first_name = firstname
     user.last_name = lastname
-    groups = [Group.objects.get(name=x) for x in settings.SAML2_AUTH.get('NEW_USER_PROFILE', {}).get('USER_GROUPS', [])]
-    if parse_version(get_version()) >= parse_version('2.0'):
-        user.groups.set(groups)
-    else:
-        user.groups = groups
-    user.is_active = settings.SAML2_AUTH.get('NEW_USER_PROFILE', {}).get('ACTIVE_STATUS', True)
-    user.is_staff = settings.SAML2_AUTH.get('NEW_USER_PROFILE', {}).get('STAFF_STATUS', True)
-    user.is_superuser = settings.SAML2_AUTH.get('NEW_USER_PROFILE', {}).get('SUPERUSER_STATUS', False)
+
+    # Obtain the Customer group instance
+    group = Group.objects.get(name='Customers')
+
+    # Set user properties according to SAML2_AUTH configuration
+    group.user_set.add(user)
+    user.is_active = settings.SAML2_AUTH.get('NEW_USER_PROFILE', {}).get('ACTIVE_STATUS', True) # Default to true if not found
+    user.is_staff = settings.SAML2_AUTH.get('NEW_USER_PROFILE', {}).get('STAFF_STATUS', False) # Default to false if not found
+    user.is_superuser = settings.SAML2_AUTH.get('NEW_USER_PROFILE', {}).get('SUPERUSER_STATUS', False) # Default to false if not found
+
+    # Save changes to the new user instance
     user.save()
     return user
 
+def _tch_setup_lead_creator(user_email):
+    logger.debug('_tch_setup_lead_creator')
+    # Get the user based on the credentials passed
+    user = User.objects.get(username=user_email)
+    # Assign the user to a new lead creator instance within the TCH organization
+    logger.debug('Making lead creator for TCH user %s' % user.id)
+    # Obtain the TCH organization instance
+    org = Organization.objects.get(name="Texas Children's Hospital - Pathology")
+    # Pass data to serializer
+    leadCreator = ReferralCreatorSerializer(data={"organization": org.id, "django_user": user.id})
+
+    # If the serializer is valid, save the leadCreator instance
+    if leadCreator.is_valid():
+        leadCreator.save()
+        logger.debug('Success')
+    else:
+        logger.debug('Failure')
+        
+    return user
+
+def _authenticate_without_password(user_email):
+    logger.debug('_authenticate_without_password')
+    # Obtain the user instance by referencing the email key
+    user = User.objects.get(email=user_email)
+    # If the user DNE, return None
+    if (user == None):
+        logger.debug('No user exists')
+    else:
+        user = PasswordlessAuthBackend.authenticate(username=user.username)
+        logger.info('user %s authenticated' % user.id)
+
+    return user
 
 @csrf_exempt
 def acs(r):
+    logger.debug('acs')
     saml_client = _get_saml_client(get_current_domain(r))
     resp = r.POST.get('SAMLResponse', None)
-    next_url = r.session.get('login_next_url', _default_next_url())
+    next_url = r.session.get('login_next_url', settings.SAML2_AUTH.get('DEFAULT_NEXT_URL', get_reverse('start_page')))
 
     if not resp:
         return HttpResponseRedirect(get_reverse([denied, 'denied', 'django_saml2_auth:denied']))
 
-    authn_response = saml_client.parse_authn_request_response(
-        resp, entity.BINDING_HTTP_POST)
+    authn_response = saml_client.parse_authn_request_response(resp, entity.BINDING_HTTP_POST)
     if authn_response is None:
         return HttpResponseRedirect(get_reverse([denied, 'denied', 'django_saml2_auth:denied']))
-
+    
     user_identity = authn_response.get_identity()
     if user_identity is None:
         return HttpResponseRedirect(get_reverse([denied, 'denied', 'django_saml2_auth:denied']))
 
-    user_email = user_identity[settings.SAML2_AUTH.get('ATTRIBUTES_MAP', {}).get('email', 'Email')][0]
-    user_name = user_identity[settings.SAML2_AUTH.get('ATTRIBUTES_MAP', {}).get('username', 'UserName')][0]
-    user_first_name = user_identity[settings.SAML2_AUTH.get('ATTRIBUTES_MAP', {}).get('first_name', 'FirstName')][0]
-    user_last_name = user_identity[settings.SAML2_AUTH.get('ATTRIBUTES_MAP', {}).get('last_name', 'LastName')][0]
+    # For Azure Active Directory Mapping
+    user_email = user_identity[settings.SAML2_AUTH.get('ATTRIBUTES_MAP', {}).get('email', 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress')][0]
+    user_name = user_identity[settings.SAML2_AUTH.get('ATTRIBUTES_MAP', {}).get('username', 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress')][0]
+    user_first_name = user_identity[settings.SAML2_AUTH.get('ATTRIBUTES_MAP', {}).get('first_name', 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/displayname')][0]
+    user_last_name = user_identity[settings.SAML2_AUTH.get('ATTRIBUTES_MAP', {}).get('last_name', 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/displayname')][0]
 
     target_user = None
     is_new_user = False
 
+    logger.debug('All user credentials received')
+    # Try to query the user by the username
     try:
         target_user = User.objects.get(username=user_name)
-        if settings.SAML2_AUTH.get('TRIGGER', {}).get('BEFORE_LOGIN', None):
-            import_string(settings.SAML2_AUTH['TRIGGER']['BEFORE_LOGIN'])(user_identity)
+    # If the User DNE, create a new user with their provided credentials
     except User.DoesNotExist:
-        new_user_should_be_created = settings.SAML2_AUTH.get('CREATE_USER', True)
-        if new_user_should_be_created: 
-            target_user = _create_new_user(user_name, user_email, user_first_name, user_last_name)
-            if settings.SAML2_AUTH.get('TRIGGER', {}).get('CREATE_USER', None):
-                import_string(settings.SAML2_AUTH['TRIGGER']['CREATE_USER'])(user_identity)
-            is_new_user = True
-        else:
-            return HttpResponseRedirect(get_reverse([denied, 'denied', 'django_saml2_auth:denied']))
+        # Create a new user
+        target_user = _create_new_user(user_name, user_email, user_first_name, user_last_name)
 
     r.session.flush()
 
+    # For TCH, assign to a lead creator instance
+    target_user = _tch_setup_lead_creator(user_email)
+
+    # If the user is active, we want to login
     if target_user.is_active:
-        target_user.backend = 'django.contrib.auth.backends.ModelBackend'
+        # Authenticate the user
+        target_user = _authenticate_without_password(user_email)
         login(r, target_user)
     else:
         return HttpResponseRedirect(get_reverse([denied, 'denied', 'django_saml2_auth:denied']))
 
-    if settings.SAML2_AUTH.get('USE_JWT') is True:
-        # We use JWT auth send token to frontend
-        jwt_token = jwt_encode(target_user)
-        query = '?uid={}&token={}'.format(target_user.id, jwt_token)
-
-        frontend_url = settings.SAML2_AUTH.get(
-            'FRONTEND_URL', next_url)
-
-        return HttpResponseRedirect(frontend_url+query)
-
     if is_new_user:
         try:
-            return render(r, 'django_saml2_auth/welcome.html', {'user': r.user})
+            return render(r, 'django_saml2_auth/tchWelcome.html', {'user': r.user})
         except TemplateDoesNotExist:
             return HttpResponseRedirect(next_url)
     else:
         return HttpResponseRedirect(next_url)
-
 
 def signin(r):
     try:
@@ -234,7 +256,7 @@ def signin(r):
             next_url = _urlparse.parse_qs(_urlparse.urlparse(unquote(next_url)).query)['next'][0]
     except:
         next_url = r.GET.get('next', _default_next_url())
-
+    
     # Only permit signin requests where the next_url is a safe URL
     if parse_version(get_version()) >= parse_version('2.0'):
         url_ok = is_safe_url(next_url, None)
@@ -255,9 +277,8 @@ def signin(r):
         if key == 'Location':
             redirect_url = value
             break
-
+    
     return HttpResponseRedirect(redirect_url)
-
 
 def signout(r):
     logout(r)
