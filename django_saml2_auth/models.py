cdc504d9f3a9d6b6ec313686141816344c5b2d05
Jack Manthorp <jack@passthrough.com>
2023-03-08 16:18:25 -0500

    add in help text for new field

--- django_saml2_auth/models.py
+++ django_saml2_auth/models.py
@@ -12,8 +12,20 @@ class SamlMetaData(models.Model):
     metadata_contents = models.TextField()
     email_domain = models.TextField(unique=True)
 
-    enable_saml = models.BooleanField(blank=True, null=True,default=False)
-    enable_optional_saml = models.BooleanField(blank=True, null=True,default=False)
+    enable_saml = models.BooleanField(
+        blank=True, 
+        null=True,
+        default=False,
+    )
+    enable_optional_saml = models.BooleanField(
+        blank=True, 
+        null=True,
+        default=False,
+        help_text=(
+            "when False, SAML is forced if it is enabled. "
+            "When True, SAML is optional when it is enabled."
+        ),
+    )
 
     def __str__(self):
         return f"<SAML Metadata: {self.email_domain}>"
